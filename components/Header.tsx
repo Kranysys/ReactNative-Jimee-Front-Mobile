302a4964c111ee5f41f9a1d2d153a099a3b459e8
Nicolas Baptista <33665069+Kranysys@users.noreply.github.com>
2019-10-02 22:57:25 +0200

    trick maj

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Text, TouchableOpacity, StyleSheet, View, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { getInstaAccount, getUserInstaID } from '../api';
 
 export default class Header extends Component {
     render() {
         return(
             <View style={{marginBottom: 55}}>
-                <TouchableOpacity onPress={ () => { this.props.navigation.openDrawer(); } } style={{width: 50, height: 50, position: 'absolute', top: 15, left: 20}}>
+                <TouchableOpacity onPress={ () => { this.props.this.props.navigation.openDrawer(); } } style={{width: 50, height: 50, position: 'absolute', top: 15, left: 20}}>
                     <Image source={require('../assets/images/menu.png')} />
                 </TouchableOpacity>
                 <Text style={{fontSize: 30, fontWeight: '700', position: 'absolute', top: 7, left: 85, fontFamily: 'Roboto'}}>{this.props.title}</Text>
                 <Image
-                style={{width: 55, height: 55, borderWidth: 1, borderRadius: 10, borderColor: '#ccc', position: 'absolute', top: 8, right: 10 }}
-                source={{uri: getInstaAccount(this.instaAccountsContentID[key]).avatar}}
+                style={{width: 50, height: 50, borderWidth: 1, borderRadius: 25, borderColor: '#ccc', position: 'absolute', top: 8, right: 10 }}
+                source={{uri: getInstaAccount(getUserInstaID()).avatar}}
                 />
             </View>
         );
--- screens/TricksScreen.js
+++ screens/TricksScreen.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View, Text, Alert, TouchableOpacity, Platform, StatusBar, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { api } from '../api';
+import Header from '../components/Header';
 
 export default class SettingsScreen extends React.Component {
   constructor(props){
@@ -15,14 +15,26 @@ export default class SettingsScreen extends React.Component {
   render() {
     return(
       <ScrollView style={styles.AndroidSafeArea}>
-        <Header title="Conseils" />
+        <Header title="Conseils" this={this}/>
         <View style={{marginTop: 35, }}>
           <Text style={styles.titre}>Inspiration Posts</Text>
           <Text style={styles.titre}>Améliorer mon profil</Text>
           <Text style={styles.titre}>Notifications & Conseils</Text>
-          <View style={styles.feed}>
 
+          <View style={styles.feed}>
+            <View style={{flexDirection: 'row', paddingBottom: 10}}>
+              <Ionicons name='md-airplane' size={15} style={{marginRight: 7}} /><Text>Feed</Text><Text style={{position: 'absolute', right: 15, fontWeight: 'bold'}}>22 sept</Text>
+            </View>
+            <Text>Et si tu ajoutais un peu plus de peps dans les textes detes posts. Un peu de smiley, une pointe d'ironie, et une petite histoire, je suis sur qu'ils vont adorer !</Text>
           </View>
+
+          <View style={styles.feed}>
+            <View style={{flexDirection: 'row', paddingBottom: 10}}>
+              <Ionicons name='md-airplane' size={15} style={{marginRight: 7}} /><Text>Feed</Text><Text style={{position: 'absolute', right: 15, fontWeight: 'bold'}}>22 sept</Text>
+            </View>
+            <Text>Ta publication a été plus performante que toutes celledu mois dernier. Bravo Michel, je te conseille de continuerà poster ce type de photo, ta communauté adore ca !</Text>
+          </View>
+
         </View>
       </ScrollView>
     );
@@ -50,12 +62,13 @@ const styles = StyleSheet.create({
     padding: 20
   },
   feed: {
+    margin: 20,
     padding: 20,
     borderRadius: 5,
     shadowColor: '#000',
-    shadowOffset: { width: 4, height: 4 },
-    shadowOpacity: 0.8,
-    shadowRadius: 4,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.6,
+    shadowRadius: 2,
     elevation: 1,
   }
 });
